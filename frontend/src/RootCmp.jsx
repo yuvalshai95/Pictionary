05af239a6392b5c2396a33ad4c4547dd332fd3f3
Yuval <yuvalshai95@gmail.com>
2022-03-06 20:12:31 +0200

    *Welcome page css

--- frontend/src/RootCmp.jsx
+++ frontend/src/RootCmp.jsx
@@ -5,14 +5,12 @@ import { Routes, Route } from 'react-router-dom';
 // Cmps
 import { Welcome } from './pages/Welcome'
 import { Drawing } from './pages/Drawing'
-import { AppHeader } from './cmps/AppHeader';
 import { UserMsg } from './cmps/UserMsg';
 
 export function RootCmp() {
 
   return (
     <div className='root-cmp'>
-      <AppHeader />
       <main>
         <Routes>
           <Route path='/' element={<Welcome />} />
--- frontend/src/pages/Welcome.jsx
+++ frontend/src/pages/Welcome.jsx
@@ -1,7 +1,13 @@
 import { useEffect, useState, useRef } from 'react';
+import { useNavigate } from 'react-router'
+
+// Services
 import { showUserMsg } from '../services/event-bus.service.js'
 import { socketService } from '../services/socket.service.js'
-import { useNavigate } from 'react-router'
+
+// Icons
+import logo from '../assets/imgs/logo.png'
+
 export const Welcome = () => {
   const navigate = useNavigate()
   const inputRef = useRef()
@@ -14,23 +20,39 @@ export const Welcome = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!name) {
-      console.log('empty name');
-      return
+      showUserMsg('You must enter a name!', 'warning')
     } else {
       socketService.emit('join', name)
-      // redirect to choose word
       navigate('/drawing')
     }
   }
 
   return (
-    <section className="home-page">
-      <h1>Welcome</h1>
-      <form onSubmit={handleSubmit}>
-        <label>Name: </label>
-        <input ref={inputRef} type="text" value={name} onChange={(e) => { setName(e.target.value) }} />
-        <button type="submit"> start </button>
-      </form>
+    <section className="home-page flex column align-center">
+      <div className="header flex justify-center align-center">
+        <div className="logo">
+          <p>Pictionary</p>
+          <img src={logo} alt="" />
+        </div>
+      </div>
+      <div className="form-wrapper flex column justify-center align-center">
+        <h1>Enter your name to play</h1>
+        <form className="form flex column  align-center" onSubmit={handleSubmit}>
+          <input
+            className="form-input"
+            ref={inputRef}
+            type="text"
+            value={name}
+            onChange={(e) => { setName(e.target.value) }}
+            placeholder="Enter name..."
+          />
+          <button
+            className="primary-btn"
+            type="submit">
+            start
+          </button>
+        </form>
+      </div>
     </section>
   );
 }
